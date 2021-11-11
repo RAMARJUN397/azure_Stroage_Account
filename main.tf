77c7ec8e934ade3ae6825e4680e9b2c50f797bf4
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-11 12:26:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,11 +19,29 @@ resource "azurerm_resource_group" "Terraform_Resource" {
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "Terraform-Network" {
-  name                = "Terraform-Network"
+resource "azurerm_virtual_network" "TNetwork" {
+  name                = "TNetwork"
   resource_group_name = "Terraform_Resource"
   location            = "East US"
   address_space       = ["10.0.0.0/16"]
+}
+resource "azurerm_subnet" "tsubnet" {
+  name                 = "tsubnet"
+  resource_group_name  = "Terraform_Resource"
+  virtual_network_name = "TNetwork" 
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = "East US"
+  resource_group_name = "Terraform_Resource"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "tsubnet"
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_windows_virtual_machine" "Terraform_Console" {
@@ -33,5 +51,19 @@ resource "azurerm_windows_virtual_machine" "Terraform_Console" {
   size                = "Standard_F2"
   admin_username      = "adminuser"
   admin_password      = "P@$$w0rd1234!"
-  
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
