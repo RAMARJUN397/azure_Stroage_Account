da40873ea14aa7cea66bb9ce031acfa6a01467f6
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-11 17:07:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
 
- terraform {
-  required_version = ">=0.12.13"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.84.0"
+    }
+  }
 }
 
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  client_id =  "3e97cc07-1df9-4433-bc68-030a1a78b255"
-  client_secret =  "bdc386aa-69db-441f-b620-c7095e9f9865"
   subscription_id = "3d133d92-97d4-4100-96d8-3a6e163da246"
-  tenant_id =  "14ad6a30-731f-42b0-8013-b85efecc3073"
+  tenant_id = "14ad6a30-731f-42b0-8013-b85efecc3073"
+  client_id = "e9081202-5020-4a9c-828c-29b0e5e64958"
+  client_secret = "b6d7abba-5ad5-4c16-bfcb-96c3cacaeb3f"
 }
-
 # Create a resource group
 resource "azurerm_resource_group" "TResource" {
   name     = "Terraform_Resource"
