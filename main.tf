76b9de54e8ae48685ee6a61434cfc2ca2655cf77
mallikarjunav747@gmail.com <mallikarjunav747@gmail.com>
2021-11-11 11:51:45 +0530

    Azure Terraform Demo

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "Terraform_Resource" {
+  name     = "Terraform_Resource"
+  location = "East US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "Terraform-Network" {
+  name                = "Terraform-Network"
+  resource_group_name = "Terraform_Resource"
+  location            = "East US"
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_windows_virtual_machine" "Terraform_Console" {
+  name                = "Terraform_Console"
+  resource_group_name = "Terraform_Resource"
+  location            = "East US"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  
+}
