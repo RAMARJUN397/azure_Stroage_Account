08c631d1163cc70a0c0ce95979747158055edfab
RAMARJUN397 <69568813+RAMARJUN397@users.noreply.github.com>
2021-11-11 12:17:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "2.46.0"
     }
   }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  version = "=2.0.0"
   features {}
 }
 
